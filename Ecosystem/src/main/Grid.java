124dfaeab1385e244ade0977b47a88187d06bd29
DavidDinkevich <david.dinkevich@gmail.com>
2018-04-13 14:28:30 -0400

    Grid: initial commit...
    1. Grid is a new class to divide the world map into cells. In this Grid
    class, the Bin-Lattice Spatial Subdivision optimization algorithm will
    be implemented

---
+++ Ecosystem/src/main/Grid.java
@@ -0,0 +1,76 @@
+package main;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Grid extends GraphicsObject {
+	private Cell[][] cells;
+	
+	public Grid(Dimension s, int numCells) {
+		size.set(s);
+		color.set(0f);
+		final int sideNumCells = (int)Math.sqrt(numCells);
+		cells = new Cell[sideNumCells][sideNumCells];
+
+		for (int x = 0; x < sideNumCells; x++) {
+			for (int y = 0; y < sideNumCells; y++) {
+				cells[x][y] = new Cell(getCellSize());
+			}
+		}
+	}
+	
+	@Override
+	public void display(Canvas c) {
+		super.display(c);
+		
+		c.strokeWeight(10);
+		c.noFill();
+		final float size = this.size.getWidth();
+		c.rect(0f, 0f, size, size);
+		c.strokeWeight(1f);
+		
+		Dimension cellSize = getCellSize();
+		
+		for (float x = -size/2f; x < size/2f; x += cellSize.getWidth()) {
+			c.line(x, -size/2f, x, size/2f);
+		}
+		for (float y = -size/2f; y < size/2f; y += cellSize.getHeight()) {
+			c.line(-size/2f, y, size/2f, y);
+		}
+	}
+	
+	@Override
+	public void update(Canvas c) {
+		Vec2 mouse = c.getMouseLocOnGrid();
+//		System.out.println(Arrays.toString(getCell(mouse)));
+		System.out.println(getCell(mouse)[0] + ", " + getCell(mouse)[1]);
+	}
+
+	@Override
+	public boolean containsPoint(Vec2 point) {
+		return false;
+	}
+	
+	public int[] getCell(Vec2 point) {
+		return new int[] { getSideCellCount()/2 + (int)Math.floor(point.x/getCellSize().getWidth()), (int)Math.floor(point.getY()/100f) };
+	}
+	
+	public int getSideCellCount() {
+		return cells.length;
+	}
+	
+	public Dimension getCellSize() {
+		return Dimension.div(size, getSideCellCount(), false);
+	}
+	
+	private static class Cell {
+		private List<GraphicsObject> elements;
+		private Dimension.Mutable size;
+		
+		public Cell(Dimension size) {
+			this.size = new Dimension.Mutable(size);
+			elements = new LinkedList<>();
+		}
+	}
+}
