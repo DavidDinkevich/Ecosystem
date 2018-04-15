15be085fc6af728b168a02e6eaea07196b94f4a7
DavidDinkevich <david.dinkevich@gmail.com>
2018-04-14 21:14:26 -0400

    EcosystemMain: now uses Swing, not JavaFX, and loads the Simulation

--- Ecosystem/src/main/EcosystemMain.java
+++ Ecosystem/src/main/EcosystemMain.java
@@ -1,35 +1,16 @@
 package main;
 
-import java.io.File;
-
-import javax.swing.SwingUtilities;
-
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.stage.Stage;
-
-public final class EcosystemMain extends Application {
-//	private EcosystemMain() {
-//		throw new AssertionError();
-//	}
+public class EcosystemMain {
 	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		File file = Saver.promptUserToLoadSimulation("data", primaryStage);
-		if (file != null) {
-			System.out.println(file.getPath());
-		}
-		Platform.exit();
-	}
-
 	public static void main(String[] args) {
-		launch(args);
-		System.out.println("Here");
-
+		Simulation sim = Saver.promptUserToLoadSimulation("data");
 		
-		SwingUtilities.invokeLater(() -> {
-//			new MainWindow();
-		});
+		// Create a new simulation from scratch
+		if (sim == null) {
+			new MainWindow(null);
+		} else {
+			new MainWindow(sim);
+		}
 	}
 
 }
