05583b1844426d1264ae91ce1c873f59613675d1
DavidDinkevich <david.dinkevich@gmail.com>
2018-04-16 14:58:09 -0400

    EcysystemMain: everything is now run on the AWT Event Dispatch Thread

--- Ecosystem/src/main/EcosystemMain.java
+++ Ecosystem/src/main/EcosystemMain.java
@@ -1,16 +1,20 @@
 package main;
 
+import javax.swing.SwingUtilities;
+
 public class EcosystemMain {
 	
 	public static void main(String[] args) {
-		Simulation sim = Saver.promptUserToLoadSimulation();
-		
-		// Create a new simulation from scratch
-		if (sim == null) {
-			new MainWindow(null);
-		} else {
-			new MainWindow(sim);
-		}
+		SwingUtilities.invokeLater(() -> {
+			Simulation sim = Saver.promptUserToLoadSimulation();
+			
+			// Create a new simulation from scratch
+			if (sim == null) {
+				new MainWindow(null);
+			} else {
+				new MainWindow(sim);
+			}
+		});
 	}
 
 }
