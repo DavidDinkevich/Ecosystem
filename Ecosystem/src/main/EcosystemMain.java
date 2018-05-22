0a072a9b727593e79888102116b5de53cc4c7e28
DavidDinkevich <david.dinkevich@gmail.com>
2018-05-22 11:17:25 -0400

    Program now uses cross-platform LaF

--- Ecosystem/src/main/EcosystemMain.java
+++ Ecosystem/src/main/EcosystemMain.java
@@ -1,13 +1,26 @@
 package main;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class EcosystemMain {
 	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
+			// Sets the background color of JToggleButtons that are selected
+			// to black
+			UIManager.put("ToggleButton.select", new java.awt.Color(0));
+
+			// Load Simulation
 			Simulation sim = Saver.promptUserToLoadSimulation();
-			
+
+			// Change look and feel
+			try {
+				UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 			// Create a new simulation from scratch
 			if (sim == null) {
 				new MainWindow(null);
