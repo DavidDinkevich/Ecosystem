24a19c6131550935b03e8779e8a4ca4ce29fcba9
DavidDinkevich <david.dinkevich@gmail.com>
2018-05-22 12:03:34 -0400

    Added to "graphics" package

---
+++ Ecosystem/src/graphics/GraphicsEllipse.java
@@ -0,0 +1,30 @@
+package graphics;
+
+import utils.Vec2;
+
+public abstract class GraphicsEllipse extends GraphicsObject {
+
+	private static final long serialVersionUID = 2910760275414600677L;
+
+	@Override
+	public boolean equals(Object o) {
+		return super.equals(o) && o instanceof GraphicsEllipse;
+	}
+	
+	@Override
+	public void display(Canvas c) {
+		c.noStroke();
+		c.fill(color);
+		c.ellipse(loc, size);
+	}
+	
+	@Override
+	public boolean containsPoint(Vec2 point) {
+		return Vec2.dist(point, loc) <= getSize().getWidth()/2f;
+	}
+	
+	public boolean intersects(GraphicsEllipse other) {
+		return Vec2.dist(loc, other.getLoc()) < 
+				getSize().getWidth()/2f + other.getSize().getWidth()/2f;
+	}
+}
