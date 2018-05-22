c8a9098f380cdc7615cd46252bdffb675a5fe8ad
DavidDinkevich <david.dinkevich@gmail.com>
2018-05-22 13:22:41 -0400

    FoodStatsPanel: Initial commit

---
+++ Ecosystem/src/graphics/ui/FoodStatsPanel.java
@@ -0,0 +1,64 @@
+package graphics.ui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
+
+import simelements.Food;
+
+public class FoodStatsPanel extends JPanel {
+
+	private static final long serialVersionUID = 8280311947731112920L;
+
+	private Food food;
+	
+	private JLabel titleLabel;
+	private JLabel foodIDLabel;
+	private JLabel foodAmtLabel, foodAmtValueLabel;
+	private JLabel numAgentsLabel, numAgentsValueLabel;
+	
+	public FoodStatsPanel(MainWindow parentWindow, Food food) {
+		this.food = food;
+		
+		setPreferredSize(new Dimension(300, parentWindow.getHeight()));
+		setLayout(new MigLayout("", "[]26[]", "[]10[]"));
+		
+		titleLabel = new JLabel("Food");
+		foodIDLabel = new JLabel();
+		titleLabel.setFont(new Font("Arial", Font.BOLD, 40));
+		foodIDLabel.setFont(new Font("Arial", Font.BOLD, 24));
+		foodIDLabel.setForeground(new Color(150, 150, 150));
+		add(titleLabel);
+		add(foodIDLabel, "wrap");
+		foodAmtLabel = new JLabel("Content:");
+		foodAmtValueLabel = new JLabel();
+		add(foodAmtLabel);
+		add(foodAmtValueLabel, "wrap");
+		numAgentsLabel = new JLabel("Num Agents:");
+		numAgentsValueLabel = new JLabel();
+		add(numAgentsLabel);
+		add(numAgentsValueLabel, "wrap");
+	}
+	
+	public Food getFood() {
+		return food;
+	}
+	
+	public void setFood(Food food) {
+		this.food = food;
+		updateStats();
+	}
+	
+	public void updateStats() {
+		if (food != null) {
+			foodAmtValueLabel.setText(food.getValue() + " / " + food.getMaxValue());
+			numAgentsValueLabel.setText("" + food.getAgents().size());
+		}
+	}
+	
+}
